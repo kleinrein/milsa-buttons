a4019a91a70e048157e8c77aac29278a8ae627ae
Andreas Rein <rein@live.no>
2017-10-09 20:44:51 +0200

    Init

---
+++ config.rb
@@ -0,0 +1,14 @@
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "/"
+javascripts_dir = "/"
+output_style = :expanded
+line_comments = true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
