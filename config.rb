dbbdd81bbe43a2b65ec38436573e9976ded43459
Andreas Rein <rein@live.no>
2017-10-09 20:59:56 +0200

    Output improvements

--- config.rb
+++ config.rb
@@ -4,11 +4,5 @@ sass_dir = "/"
 images_dir = "/"
 javascripts_dir = "/"
 output_style = :expanded
-line_comments = true
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+#output_style = :compressed
+line_comments = false
